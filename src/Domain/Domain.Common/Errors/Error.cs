163430392f166d271f24d657f605f2fcecea37fb
xvsvg <pobedotriples@gmail.com>
2023-10-27 09:35:31 +0300

    feat: domain exception

---
+++ src/Domain/Domain.Common/Errors/Error.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Common.Abstractions;
+
+namespace Domain.Common.Errors;
+
+public sealed class Error : ValueObject
+{
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; }
+    public string Message { get; }
+
+    public static implicit operator string(Error error)
+    {
+        return error?.Code ?? string.Empty;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Code;
+        yield return Message;
+    }
+}
