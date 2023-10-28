6a8d14351222ecce3f9c121f0b9ca9f397effa66
constadry <behappydtworry@gmail.com>
2023-10-28 20:12:04 +0300

    feat: added queue domain logic

---
+++ src/Domain/Domain.Core/ValueObjects/QueueDay.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Core.ValueObjects;
+
+public class QueueDay
+{
+    public const int Week = 7;
+
+    private QueueDay(DateTime value) => Value = value;
+
+    public DateTime Value { get; }
+
+    public static QueueDay Create(DateTime value)
+    {
+        if (value <= DateTime.Now)
+        {
+            throw new ArgumentException("Day of queue should be later than now");
+        }
+
+        if (value >= DateTime.Now.AddDays(Week))
+        {
+            throw new ArgumentException("Day of queue should be on this week");
+        }
+
+        return new QueueDay(value);
+    }
+}
