51f7ef09d2a340474d9ef570fcd51ad27a60f6c1
xvsvg <pobedotriples@gmail.com>
2023-10-31 00:30:40 +0300

    feat: user entity

---
+++ src/Domain/Domain.Core/User/Events/UserRegisteredDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common.Abstractions;
+
+namespace Domain.Core.User.Events;
+
+public sealed class UserRegisteredDomainEvent : IDomainEvent
+{
+    public UserRegisteredDomainEvent(UserEntity user)
+    {
+        User = user;
+    }
+
+    public UserEntity User { get; }
+}
