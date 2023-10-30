3208f5977424e07f72596b396ad1f9ce6242b5c3
xvsvg <pobedotriples@gmail.com>
2023-10-30 23:19:16 +0300

    feat: remove nunit

--- tests/Test.Core/QueueDayTest.cs
+++ tests/Test.Core/QueueDayTest.cs
@@ -1,27 +1,27 @@
 ﻿using Domain.Core.ValueObjects;
-using NUnit.Framework;
+using Xunit;
 
 namespace Test.Core;
 
 public class QueueDayTest
 {
-    [Test]
+    [Fact]
     public void TestCreatingDayWithDateBeforeThisDay()
     {
         var dateBeforeThisDay = new DateTime(2007, 01, 02, 12, 16, 00);
         Assert.Catch<ArgumentException>(() =>
         {
-            var testDay = QueueDay.Create(dateBeforeThisDay);
+            var testDay = JSType.Date.Create(dateBeforeThisDay);
         });
     }
     
-    [Test]
+    [Fact]
     public void TestCreatingDayWithDateAfterThisDay()
     {
         var dateAfterThisDay = new DateTime(2024, 01, 02, 12, 16, 00);
         Assert.Catch<ArgumentException>(() =>
         {
-            var testDay = QueueDay.Create(dateAfterThisDay);
+            var testDay = JSType.Date.Create(dateAfterThisDay);
         });
     }
 }
