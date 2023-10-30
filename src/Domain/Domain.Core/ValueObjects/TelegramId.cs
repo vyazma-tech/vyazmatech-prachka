139d7326dc52028e76c4838c84f8f5ed568cda44
xvsvg <pobedotriples@gmail.com>
2023-10-31 00:30:33 +0300

    feat: user value objects

---
+++ src/Domain/Domain.Core/ValueObjects/TelegramId.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using Domain.Common.Abstractions;
+using Domain.Common.Errors;
+using Domain.Common.Exceptions;
+using LanguageExt.Common;
+
+namespace Domain.Core.ValueObjects;
+
+public sealed class TelegramId : ValueObject
+{
+    private const string DigitNumberPattern = @"^\d*$";
+
+    private TelegramId(string value)
+        => Value = value;
+
+    public string Value { get; }
+
+    public static Result<TelegramId> Create(string telegramId)
+    {
+        if (string.IsNullOrWhiteSpace(telegramId))
+        {
+            var exception = new DomainException(DomainErrors.TelegramId.NullOrEmpty);
+            return new Result<TelegramId>(exception);
+        }
+
+        if (Regex.IsMatch(telegramId, DigitNumberPattern) is false)
+        {
+            var exception = new DomainException(DomainErrors.TelegramId.InvalidFormat);
+            return new Result<TelegramId>(exception);
+        }
+
+        return new TelegramId(telegramId);
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
---
+++ src/Domain/Domain.Core/ValueObjects/UserRegistrationDate.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Common.Abstractions;
+using Domain.Common.Errors;
+using Domain.Common.Exceptions;
+using LanguageExt.Common;
+
+namespace Domain.Core.ValueObjects;
+
+public sealed class UserRegistrationDate : ValueObject
+{
+    private UserRegistrationDate(DateTime value)
+    {
+        Value = value;
+    }
+
+    public DateTime Value { get; }
+
+    public static Result<UserRegistrationDate> Create(DateTime dateTimeUtc, IDateTimeProvider dateTimeProvider)
+    {
+        if (dateTimeUtc <= dateTimeProvider.UtcNow)
+        {
+            var exception = new DomainException(DomainErrors.UserRegistrationDate.InThePast);
+            return new Result<UserRegistrationDate>(exception);
+        }
+
+        return new UserRegistrationDate(dateTimeUtc);
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
