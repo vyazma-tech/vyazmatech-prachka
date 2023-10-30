82279de84c58024fafe3104ae6449c04965db7ad
xvsvg <pobedotriples@gmail.com>
2023-10-31 00:50:25 +0300

    feat: date time provider

---
+++ src/Infrastructure/Infrastructure.Tools/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Common.Abstractions;
+
+namespace Infrastructure.Tools;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTimeOffset UtcNow => DateTimeOffset.Now;
+}
