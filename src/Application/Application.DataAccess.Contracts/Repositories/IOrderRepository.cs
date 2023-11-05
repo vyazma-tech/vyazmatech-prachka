4d1c7e78883e4317edd3262ff09b2c3f305aad50
xvsvg <pobedotriples@gmail.com>
2023-11-05 21:35:32 +0300

    feat: data access contracts

---
+++ src/Application/Application.DataAccess.Contracts/Repositories/IOrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common.Result;
+using Domain.Core.Order;
+using Domain.Core.User;
+
+namespace Application.DataAccess.Contracts.Repositories;
+
+public interface IOrderRepository
+{
+    Task<Result<OrderEntity>> FindByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    Task<Result<IReadOnlyCollection<OrderEntity>>> FindByCreationDateAsync(DateTime creationDateUtc, CancellationToken cancellationToken);
+
+    Task<Result<IReadOnlyCollection<OrderEntity>>> FindByUserAsync(UserEntity user, CancellationToken cancellationToken);
+    
+    Task InsertRangeAsync(IReadOnlyCollection<OrderEntity> orders, CancellationToken cancellationToken);
+
+    Task UpdateAsync(OrderEntity order, CancellationToken cancellationToken);
+
+    Task<long> CountAsync(CancellationToken cancellationToken);
+}
---
+++ src/Application/Application.DataAccess.Contracts/Repositories/IQueueRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common.Result;
+using Domain.Core.Order;
+using Domain.Core.Queue;
+
+namespace Application.DataAccess.Contracts.Repositories;
+
+public interface IQueueRepository
+{
+    Task<Result<QueueEntity>> FindByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    Task<Result<QueueEntity>> FindByOrderAsync(OrderEntity order, CancellationToken cancellationToken);
+
+    Task<Result<QueueEntity>> FindByCreationDate(DateTime creationDateUtc, CancellationToken cancellationToken);
+
+    Task InsertRangeAsync(IReadOnlyCollection<QueueEntity> queues, CancellationToken cancellationToken);
+
+    Task UpdateAsync(QueueEntity queue, CancellationToken cancellationToken);
+
+    Task<long> CountAsync(CancellationToken cancellationToken);
+}
