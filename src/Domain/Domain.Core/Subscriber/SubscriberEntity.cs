7e17ab52e3336495c512d1c9b59ee9bc1ab8ec87
xvsvg <pobedotriples@gmail.com>
2023-11-05 20:27:12 +0300

    refactor: subscriber tests

--- src/Domain/Domain.Core/Subscriber/SubscriberEntity.cs
+++ src/Domain/Domain.Core/Subscriber/SubscriberEntity.cs
@@ -32,9 +32,9 @@ public sealed class SubscriberEntity : Entity, IAuditableEntity
         _orders = new HashSet<OrderEntity>();
     }
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS8618
     private SubscriberEntity()
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning restore CS8618
     {
         _orders = new HashSet<OrderEntity>();
     }
