cd81ee24eba92fb7434483405f5d87ee663ced6a
xvsvg <pobedotriples@gmail.com>
2023-11-05 22:05:07 +0300

    feat: database context

---
+++ src/Infrastructure/Infrastructure.DataAccess/Contexts/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Application.DataAccess.Contracts;
+using Domain.Core.Order;
+using Domain.Core.Queue;
+using Domain.Core.Subscriber;
+using Domain.Core.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Infrastructure.DataAccess.Contexts;
+
+public sealed class DatabaseContext : DbContext, IUnitOfWork
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<OrderEntity> Orders { get; private init; } = null!;
+
+    public DbSet<UserEntity> Users { get; private init; } = null!;
+
+    public DbSet<QueueEntity> Queues { get; private init; } = null!;
+
+    public DbSet<SubscriberEntity> Subscriptions { get; private init; } = null!;
+
+    public Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default)
+        => Database.BeginTransactionAsync(cancellationToken);
+}
