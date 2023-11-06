76bc0006ebcf8431e9c6bc282b62b53335995fd1
xvsvg <pobedotriples@gmail.com>
2023-11-06 22:54:43 +0300

    feat: value converters

---
+++ src/Infrastructure/Infrastructure.DataAccess/ValueConverters/CapacityValueConverter.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Core.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.DataAccess.ValueConverters;
+
+public sealed class CapacityValueConverter : ValueConverter<Capacity, int>
+{
+    public CapacityValueConverter()
+        : base(
+            capacity => capacity.Value,
+            value => Capacity.Create(value).Value)
+    {
+    }
+}
---
+++ src/Infrastructure/Infrastructure.DataAccess/ValueConverters/TelegramIdValueConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using Domain.Core.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.DataAccess.ValueConverters;
+
+public sealed class TelegramIdValueConverter : ValueConverter<TelegramId, string>
+{
+    public TelegramIdValueConverter()
+        : base(
+            telegramId => telegramId.Value,
+            stringValue => TelegramId.Create(stringValue).Value)
+    {
+    }
+}
