f6b74039608cb8cd7054cbfa0d6afe44ea3eb858
xvsvg <pobedotriples@gmail.com>
2023-11-18 00:02:35 +0300

    feat: domain event handler contract

---
+++ src/Domain/Domain.Kernel/IDomainEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using Mediator;
+
+namespace Domain.Kernel;
+
+public interface IDomainEventHandler<in TDomainEvent> : INotificationHandler<TDomainEvent>
+    where TDomainEvent : IDomainEvent
+{
+}
