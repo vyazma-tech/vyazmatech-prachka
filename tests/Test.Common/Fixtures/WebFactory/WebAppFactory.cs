ca4bdea3946e06cf660814025113901d45b563b5
xvsvg <pobedotriples@gmail.com>
2023-11-22 21:40:07 +0300

    feat: web factory fixture

--- tests/Test.Common/Fixtures/WebFactory/WebAppFactory.cs
+++ tests/Test.Common/Fixtures/WebFactory/WebAppFactory.cs
@@ -1,5 +1,24 @@
-﻿namespace Test.Common.Fixtures.WebFactory;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using TrusovNET.Playground;
+using Xunit;
 
-public class WebAppFactory
+namespace Test.Common.Fixtures.WebFactory;
+
+public class WebAppFactory : WebApplicationFactory<IPlaygroundMarker>, IAsyncLifetime
 {
+    protected HttpClient Client { get; private set; } = default!;
+
+    public Task InitializeAsync()
+    {
+        Client = CreateClient();
+
+        return Task.CompletedTask;
+    }
+
+    public new Task DisposeAsync()
+    {
+        Client.Dispose();
+
+        return Task.CompletedTask;
+    }
 }
