faaef5125ff1e040017d19e6faf99c01f21db475
xvsvg <pobedotriples@gmail.com>
2023-11-26 18:25:01 +0300

    fix: handler registration

---
+++ src/Application/Application.Handlers/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Handlers.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services.AddMediator(s =>
+        {
+            s.Namespace = "Application.Handlers";
+            s.ServiceLifetime = ServiceLifetime.Transient;
+        });
+    }
+}
