05b4b88bdfe42565a96d11a096fb3439a5806d3e
xvsvg <pobedotriples@gmail.com>
2024-01-10 18:20:15 +0300

    refactor: user querying

--- src/Application/Application.Core/Extensions/FilteringExtensions.cs
+++ src/Application/Application.Core/Extensions/FilteringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Core.Queue;
+using Domain.Core.User;
 
 namespace Application.Core.Extensions;
 
@@ -9,5 +10,28 @@ public static class FilteringExtensions
         return date is null
             ? queues
             : queues.Where(x => x.CreationDate == date);
+    }
+
+    public static IEnumerable<UserEntity> FilterBy(
+        this IEnumerable<UserEntity> users,
+        string? telegramId,
+        string? fullname,
+        DateOnly? registrationDate)
+    {
+        IEnumerable<UserEntity> result = users as UserEntity[] ?? users.ToArray();
+
+        result = telegramId is null
+            ? result
+            : result.Where(x => string.Equals(x.TelegramId.Value, telegramId, StringComparison.InvariantCulture));
+
+        result = fullname is null
+            ? result
+            : result.Where(x => string.Equals(x.Fullname.Value, fullname, StringComparison.InvariantCultureIgnoreCase));
+
+        result = registrationDate is null
+            ? result
+            : result.Where(x => x.CreationDate == registrationDate);
+
+        return result;
     }
 }
--- src/Infrastructure/Infrastructure.DataAccess/Specifications/User/UserByIdSpecification.cs
+++ src/Infrastructure/Infrastructure.DataAccess/Specifications/User/UserByIdSpecification.cs
@@ -2,10 +2,11 @@
 using Domain.Core.User;
 using Domain.Kernel;
 using Infrastructure.DataAccess.Contracts;
+using Infrastructure.DataAccess.Models;
 
 namespace Infrastructure.DataAccess.Specifications.User;
 
-public sealed class UserByIdSpecification : Specification<UserEntity>
+public sealed class UserByIdSpecification : Specification<UserModel>
 {
     private readonly Guid _id;
 
