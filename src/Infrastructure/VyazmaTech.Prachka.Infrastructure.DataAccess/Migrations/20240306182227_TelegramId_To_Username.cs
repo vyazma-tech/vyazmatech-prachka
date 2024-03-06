df739028ee5c7abfbe07162078e51e85ad6bdad3
Владимир Олейников <pobedotriples@gmail.com>
2024-03-06 23:07:51 +0300

    chore: add migration & adjust .gitignore

---
+++ src/Infrastructure/VyazmaTech.Prachka.Infrastructure.DataAccess/Migrations/20240306182227_TelegramId_To_Username.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VyazmaTech.Prachka.Infrastructure.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class TelegramId_To_Username : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TelegramId",
+                table: "Users",
+                newName: "TelegramUsername");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TelegramUsername",
+                table: "Users",
+                newName: "TelegramId");
+        }
+    }
+}
