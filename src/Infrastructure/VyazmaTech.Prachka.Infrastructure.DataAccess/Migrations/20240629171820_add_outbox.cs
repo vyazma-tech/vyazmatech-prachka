67daea5a161f4eb3cf0ffb7019fb1b09e1682272
xvsvg <pobedotriples@gmail.com>
2024-06-30 19:34:08 +0300

    fix: add missing migrations

---
+++ src/Infrastructure/VyazmaTech.Prachka.Infrastructure.DataAccess/Migrations/20240629171820_add_outbox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VyazmaTech.Prachka.Infrastructure.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_outbox : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "outbox_messages",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    occured_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    processed_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    error = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_outbox_messages", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_outbox_messages_processed_on_utc",
+                table: "outbox_messages",
+                column: "processed_on_utc",
+                filter: "processed_on_utc is null");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "outbox_messages");
+        }
+    }
+}
