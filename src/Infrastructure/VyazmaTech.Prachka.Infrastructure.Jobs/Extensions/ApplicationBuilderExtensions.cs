fbfcd24b6857884f7e6b0344c6c5857f326296b0
xvsvg <pobedotriples@gmail.com>
2024-07-28 16:19:46 +0300

    [hotfix] fix dashboard

--- src/Infrastructure/VyazmaTech.Prachka.Infrastructure.Jobs/Extensions/ApplicationBuilderExtensions.cs
+++ src/Infrastructure/VyazmaTech.Prachka.Infrastructure.Jobs/Extensions/ApplicationBuilderExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using Hangfire;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.HttpOverrides;
 
 namespace VyazmaTech.Prachka.Infrastructure.Jobs.Extensions;
 
@@ -8,16 +7,6 @@ public static class ApplicationBuilderExtensions
 {
     public static IApplicationBuilder UseSchedulingDashboard(this IApplicationBuilder builder)
     {
-        var options = new ForwardedHeadersOptions
-        {
-            ForwardedHeaders = ForwardedHeaders.All
-        };
-        options.KnownNetworks.Clear();
-        options.KnownProxies.Clear();
-
-        builder.UseForwardedHeaders(options);
-
-        // builder.UsePathBase("/hangfire/");
         builder.UseHangfireDashboard(
             options: new DashboardOptions
             {
