665d5c248791b2830005f76a607d2a783ac8921d
xvsvg <pobedotriples@gmail.com>
2024-07-30 20:25:55 +0300

    [#72] ascending queue sort order

--- src/Presentation/VyazmaTech.Prachka.Presentation.Endpoints/Queue/V1/FindQueuesEndpoint.cs
+++ src/Presentation/VyazmaTech.Prachka.Presentation.Endpoints/Queue/V1/FindQueuesEndpoint.cs
@@ -5,6 +5,7 @@ using VyazmaTech.Prachka.Application.Abstractions.Configuration;
 using VyazmaTech.Prachka.Application.Contracts.Core.Queues.Queries;
 using VyazmaTech.Prachka.Application.Dto;
 using VyazmaTech.Prachka.Application.Dto.Core.Queue;
+using VyazmaTech.Prachka.Domain.Kernel;
 using VyazmaTech.Prachka.Presentation.Endpoints.Extensions;
 using VyazmaTech.Prachka.Presentation.Endpoints.Queue.V1.Models;
 
@@ -13,11 +14,16 @@ namespace VyazmaTech.Prachka.Presentation.Endpoints.Queue.V1;
 internal class FindQueuesEndpoint : Endpoint<FindQueuesRequest, PagedResponse<QueueDto>>
 {
     private readonly ISender _sender;
+    private readonly IDateTimeProvider _timeProvider;
     private readonly int _recordPerPage;
 
-    public FindQueuesEndpoint(ISender sender, IOptions<PaginationConfiguration> paginationConfiguration)
+    public FindQueuesEndpoint(
+        ISender sender,
+        IOptions<PaginationConfiguration> paginationConfiguration,
+        IDateTimeProvider timeProvider)
     {
         _sender = sender;
+        _timeProvider = timeProvider;
         _recordPerPage = paginationConfiguration.Value.RecordsPerPage;
     }
 
@@ -32,7 +38,7 @@ internal class FindQueuesEndpoint : Endpoint<FindQueuesRequest, PagedResponse<Qu
     public override async Task HandleAsync(FindQueuesRequest req, CancellationToken ct)
     {
         var query = new QueueByQuery.Query(
-            SearchFrom: req.SearchFrom,
+            SearchFrom: req.SearchFrom ?? _timeProvider.DateNow,
             Page: req.Page,
             Limit: req.Limit ?? _recordPerPage);
 
