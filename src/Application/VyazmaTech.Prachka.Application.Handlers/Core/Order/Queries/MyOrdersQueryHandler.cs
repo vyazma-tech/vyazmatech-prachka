a7809f7cc63962aca5f01534c411e42100c6c6ac
xvsvg <pobedotriples@gmail.com>
2024-07-30 22:05:35 +0300

    [#66] my orders endpoint

---
+++ src/Application/VyazmaTech.Prachka.Application.Handlers/Core/Order/Queries/MyOrdersQueryHandler.cs
@@ -0,0 +1,54 @@
+using VyazmaTech.Prachka.Application.Abstractions.Identity;
+using VyazmaTech.Prachka.Application.Contracts.Common;
+using VyazmaTech.Prachka.Application.Core.Errors;
+using VyazmaTech.Prachka.Application.DataAccess.Contracts;
+using VyazmaTech.Prachka.Application.Dto.Core.Order;
+using VyazmaTech.Prachka.Domain.Common.Exceptions;
+using static VyazmaTech.Prachka.Application.Contracts.Core.Orders.Queries.MyOrders;
+
+namespace VyazmaTech.Prachka.Application.Handlers.Core.Order.Queries;
+
+internal sealed class MyOrdersQueryHandler : IQueryHandler<Query, Response>
+{
+    private readonly ICurrentUser _currentUser;
+    private readonly IPersistenceContext _context;
+
+    public MyOrdersQueryHandler(ICurrentUser currentUser, IPersistenceContext context)
+    {
+        _currentUser = currentUser;
+        _context = context;
+    }
+
+    public async ValueTask<Response> Handle(Query request, CancellationToken cancellationToken)
+    {
+        Guid? userId = _currentUser.Id;
+
+        if (userId is null)
+            throw new IdentityException(ApplicationErrors.MyOrders.AnonymousUserCantSeeTheirOrders);
+
+        List<MyOrdersDto> orders = await _context.Orders
+            .QueryByUserAsync(userId.Value, cancellationToken)
+            .GroupBy(x => x.CreationDate)
+            .SelectAwait(async group =>
+                new MyOrdersDto(
+                    Date: group.Key,
+                    Orders: await ToOrdersModel(group, cancellationToken)))
+            .ToListAsync(cancellationToken);
+
+        return new Response(orders);
+    }
+
+    private static async Task<IReadOnlyCollection<MyOrdersOrderModel>> ToOrdersModel(
+        IAsyncGrouping<DateOnly, Domain.Core.Orders.Order> group,
+        CancellationToken token)
+    {
+        return await group.Select(order =>
+                new MyOrdersOrderModel(
+                    Status: order.Status.ToString(),
+                    Comment: order.Comment,
+                    CreationDate: order.CreationDate,
+                    ModificationDate: order.ModifiedOnUtc,
+                    isNotifyAvailable: false))
+            .ToListAsync(token);
+    }
+}
---
+++ tests/VyazmaTech.Prachka.Application.Handlers.Tests/Order/Queries/MyOrdersTests.cs
@@ -0,0 +1,98 @@
+using FluentAssertions;
+using Moq;
+using VyazmaTech.Prachka.Application.Abstractions.Identity;
+using VyazmaTech.Prachka.Application.Contracts.Core.Orders.Queries;
+using VyazmaTech.Prachka.Application.Core.Errors;
+using VyazmaTech.Prachka.Application.Core.Users;
+using VyazmaTech.Prachka.Application.Handlers.Core.Order.Queries;
+using VyazmaTech.Prachka.Application.Handlers.Tests.Fixtures;
+using VyazmaTech.Prachka.Domain.Common.Exceptions;
+using VyazmaTech.Prachka.Domain.Core.Orders;
+using VyazmaTech.Prachka.Domain.Kernel;
+using VyazmaTech.Prachka.Tests.Tools.FluentBuilders;
+
+namespace VyazmaTech.Prachka.Application.Handlers.Tests.Order.Queries;
+
+public sealed class MyOrdersTests : TestBase
+{
+    private static readonly VerifySettings Settings;
+    private readonly MyOrdersQueryHandler _handler;
+    private readonly Mock<ICurrentUser> _currentUser;
+
+    static MyOrdersTests()
+    {
+        Settings = new VerifySettings();
+        Settings.UseDirectory("Contracts");
+    }
+
+    public MyOrdersTests(CoreDatabaseFixture fixture) : base(fixture)
+    {
+        _currentUser = new Mock<ICurrentUser>();
+        _handler = new MyOrdersQueryHandler(_currentUser.Object, fixture.PersistenceContext);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrow_WhenUserIsNotAuthenticated()
+    {
+        // Arrange
+        var user = new AnonymousUser();
+        _currentUser.Setup(x => x.Id).Returns(user.Id);
+
+        // Act
+        var query = default(MyOrders.Query);
+        var action = async () => await _handler.Handle(query, default);
+
+        // Assert
+        var exception = await action.Should().ThrowAsync<IdentityException>();
+        exception.Which.Error.Should().Be(ApplicationErrors.MyOrders.AnonymousUserCantSeeTheirOrders);
+    }
+
+    [Fact]
+    public async Task Verify_Contract()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        _currentUser.Setup(x => x.Id).Returns(userId);
+
+        var user = Create.User
+            .WithId(userId)
+            .WithFullname("Bobby Shmurda")
+            .WithTelegramUsername("@bobster")
+            .Build();
+
+        var queue = Create.Queue
+            .WithCapacity(20)
+            .WithActivityBoundaries(TimeOnly.Parse("10:00"), TimeOnly.Parse("17:00"))
+            .Build();
+
+        var todayOrders = Enumerable.Range(0, 10)
+            .Select(_ =>
+                Create.Order
+                    .WithUser(user)
+                    .WithStatus(OrderStatus.New)
+                    .WithCreationDate(DateTime.Today.AsDateOnly())
+                    .Build());
+
+        var yesterdayOrders = Enumerable.Range(0, 10)
+            .Select(_ =>
+                Create.Order
+                    .WithUser(user)
+                    .WithStatus(OrderStatus.New)
+                    .WithCreationDate(DateTime.Today.AsDateOnly().AddDays(-1))
+                    .Build());
+
+        queue.BulkInsert([..todayOrders, ..yesterdayOrders]);
+
+        PersistenceContext.Users.Insert(user);
+        PersistenceContext.Queues.InsertRange([queue]);
+
+        await PersistenceContext.SaveChangesAsync(default);
+
+        // Act
+        var query = default(MyOrders.Query);
+        var response = await _handler.Handle(query, default);
+
+        // Assert
+        await Verify(response, Settings);
+    }
+}
