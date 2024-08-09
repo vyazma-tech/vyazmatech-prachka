127b6c0b693b089729ff9b198527f6225f971312
xvsvg <pobedotriples@gmail.com>
2024-08-09 18:58:39 +0300

    [#88] remove swagger from production environment

--- src/Presentation/VyazmaTech.Prachka.Presentation.Endpoints/Extensions/ServiceCollectionExtensions.cs
+++ src/Presentation/VyazmaTech.Prachka.Presentation.Endpoints/Extensions/ServiceCollectionExtensions.cs
@@ -1,32 +1,41 @@
 ﻿using FastEndpoints;
 using FastEndpoints.Swagger;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace VyazmaTech.Prachka.Presentation.Endpoints.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddEndpoints(this IServiceCollection services)
+    public static IServiceCollection AddEndpoints(this IServiceCollection services, IWebHostEnvironment environment)
     {
-        return services
-            .AddFastEndpoints()
-            .SwaggerDocument(
-                options =>
-                {
-                    options.MaxEndpointVersion = 1;
-                    options.DocumentSettings = s =>
+        services
+            .AddFastEndpoints();
+
+        if (environment.IsProduction() is false)
+        {
+            services
+                .SwaggerDocument(
+                    options =>
                     {
-                        s.DocumentName = "v1";
-                        s.Title = "API";
-                        s.Version = "v1";
-                    };
-                });
+                        options.MaxEndpointVersion = 1;
+                        options.DocumentSettings = s =>
+                        {
+                            s.DocumentName = "v1";
+                            s.Title = "API";
+                            s.Version = "v1";
+                        };
+                    });
+        }
+
+        return services;
     }
 
-    public static IApplicationBuilder UseEndpoints(this IApplicationBuilder app)
+    public static IApplicationBuilder UseEndpoints(this IApplicationBuilder app, IWebHostEnvironment environment)
     {
-        return app.UseFastEndpoints(
+        app.UseFastEndpoints(
             config =>
             {
                 config.Versioning.Prefix = "v";
@@ -34,5 +43,10 @@ public static class ServiceCollectionExtensions
                 config.Endpoints.RoutePrefix = "api";
                 config.Versioning.DefaultVersion = 1;
             });
+
+        if (environment.IsProduction() is false)
+            app.UseSwaggerGen();
+
+        return app;
     }
 }
